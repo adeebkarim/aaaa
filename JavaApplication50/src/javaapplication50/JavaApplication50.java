c7ee08912e8d9d1941d388d474eb5b9db80f3b97
AdeeB <AdeeB@192.168.1.5>
2015-11-20 21:23:46 +0500

    ist iteration

--- JavaApplication50/src/javaapplication50/JavaApplication50.java
+++ JavaApplication50/src/javaapplication50/JavaApplication50.java
@@ -17,7 +17,7 @@ public class JavaApplication50 {
     public static void main(String[] args) {
         // TODO  code application logic here
         
-        
+        System.out.print("sss");
         
         
         
