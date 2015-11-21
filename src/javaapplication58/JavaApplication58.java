b413e5c92de45143e79001fab78a92bb3398232a
adeebkarim <adeebkarim@users.noreply.github.com>
2015-11-21 20:35:53 +0500

    Update JavaApplication58.java

--- src/javaapplication58/JavaApplication58.java
+++ src/javaapplication58/JavaApplication58.java
@@ -17,7 +17,7 @@ public class JavaApplication58 {
     public static void main(String[] args) {
         // TODO code application logic here
         
-       System.out.print("");
+       System.out.print("jjj");
        
        System.out.print("");
     }
